d0acfc4b5a156cdf8f803844af69209d12a29469
nuclearMonkeys <lyndond@uci.edu>
2019-03-30 10:50:33 -0700

    Added Levels

--- Treasure Island/Assets/_Scripts/Player/SimplePlayerController.cs
+++ Treasure Island/Assets/_Scripts/Player/SimplePlayerController.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class SimplePlayerController : MonoBehaviour
 {
-    [SerializeField] private float speed = 5.0f;
+    [SerializeField] private float speed = 7.0f;
     [SerializeField] private Rigidbody rb;
 
     // Start is called before the first frame update
@@ -16,7 +16,6 @@ public class SimplePlayerController : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-
         Move();
     }
 
@@ -24,9 +23,12 @@ public class SimplePlayerController : MonoBehaviour
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
 
+        rb.velocity = (transform.forward * vertical) * speed * Time.fixedDeltaTime;
+
         transform.Translate(new Vector3(horizontal, 0, vertical) * speed * Time.deltaTime);
 
         //rb.AddForce(new Vector3(horizontal, 0.0f, vertical) * speed);
-        //rb.velocity = transform.forward * speed;
+        rb.velocity = new Vector3(horizontal * 2, 0.0f, vertical * 2) * speed;
+        //rb.MovePosition(transform.position + new Vector3(horizontal * 2, 0.0f, vertical * 2) * speed);
     }
 }
