fb04895e9e54a80b5c7e680240eda38b622c278f
dbarajas98 <49083478+dbarajas98@users.noreply.github.com>
2019-03-30 13:44:42 -0700

    finished CameraFollow script

---
+++ Treasure Island/Assets/_Scripts/Player/CameraFollow.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform target; //target is what the camera will follow (usually player)
+    public Vector3 offset = new Vector3 (0, 5, -4);
+    void LateUpdate()
+    {
+        transform.position = target.TransformPoint(offset);
+    }
+}
