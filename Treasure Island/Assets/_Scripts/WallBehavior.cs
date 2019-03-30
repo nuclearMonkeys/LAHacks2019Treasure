a04ebf0a4b7527aff7ced59d0a12aa57bd5d2c9a
nuclearMonkeys <lyndond@uci.edu>
2019-03-30 02:24:29 -0700

    Added Walls

---
+++ Treasure Island/Assets/_Scripts/WallBehavior.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallBehavior : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("PlayerBullet") || other.CompareTag("EnemyBullet")) {
+            Destroy(other.gameObject);
+        }
+    }
+}
