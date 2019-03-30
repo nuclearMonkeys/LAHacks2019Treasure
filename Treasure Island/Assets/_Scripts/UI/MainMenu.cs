61f781d43385ac67515941d0fc64110470d6301e
nuclearMonkeys <lyndond@uci.edu>
2019-03-30 02:07:37 -0700

    Added a Main Menu

---
+++ Treasure Island/Assets/_Scripts/UI/MainMenu.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void StartGame() {
+        SceneManager.LoadScene("LyndonScene");
+    }
+
+    public void QuitGame() {
+        Application.Quit();
+    }
+}
