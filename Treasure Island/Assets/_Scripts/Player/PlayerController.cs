31bdbbcd0659f1d69ff644bd74ce0d9663c3cf03
airada <jasmineirada@gmail.com>
2019-03-30 17:20:43 -0700

    enemy follows and returns to path

--- Treasure Island/Assets/_Scripts/Player/PlayerController.cs
+++ Treasure Island/Assets/_Scripts/Player/PlayerController.cs
@@ -5,36 +5,38 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
 
-public float moveSpeed;
-private Rigidbody myRigidbody;
+    public float moveSpeed;
+    private Rigidbody myRigidbody;
+    public Vector3 position;
 
-private Vector3 moveInput;
-private Vector3 moveVelocity;
+    private Vector3 moveInput;
+    private Vector3 moveVelocity;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        myRigidbody = GetComponent<Rigidbody>();
-    }
+        // Start is called before the first frame update
+        void Start()
+        {
+            myRigidbody = GetComponent<Rigidbody>();
+        }
 
-    // Update is called once per frame
-    void Update()
-    {
+        // Update is called once per frame
+        void Update()
+        {
         
-    }
+        }
 
-    //FixedUpdate is called at constant time intervals (as opposed to every frame, which can vary by machine)
-    void FixedUpdate()
-    {
-    	myRigidbody.velocity = moveVelocity;
-        Move();
-    }
+        //FixedUpdate is called at constant time intervals (as opposed to every frame, which can vary by machine)
+        void FixedUpdate()
+        {
+            position = transform.position;
+    	    myRigidbody.velocity = moveVelocity;
+            Move();
+        }
 
-    void Move()
-    {
-        moveInput = new Vector3(Input.GetAxisRaw("Horizontal"), 0f, Input.GetAxisRaw("Vertical"));
-        moveVelocity = moveInput * moveSpeed * Time.fixedDeltaTime;
-        transform.Translate(moveVelocity);
-    }
+        void Move()
+        {
+            moveInput = new Vector3(Input.GetAxisRaw("Horizontal"), 0f, Input.GetAxisRaw("Vertical"));
+            moveVelocity = moveInput * moveSpeed * Time.fixedDeltaTime;
+            transform.Translate(moveVelocity);
+        }
 
 }
